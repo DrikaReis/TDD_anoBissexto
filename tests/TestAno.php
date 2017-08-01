23e680a329804c611491fedb8a8f6d772f79c628
Adrianny <a.ingridreis@gmail.com>
2017-07-31 21:43:06 -0300

    implementacao do problema

--- tests/TestAno.php
+++ tests/TestAno.php
@@ -6,6 +6,14 @@ class TestAno
 {
     public static function bissexto($ano)
     {
-        
+      if ($ano <= 0){
+        return false;
+      } else {
+          if (($ano % 4 == 0 && $ano % 100 != 0) || $ano % 400 == 0) {
+              return true;
+          } else {
+              return false;
+          }
+      }
     }
 }
