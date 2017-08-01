6824ff3b495cd52ab33c84413535376e0888a21f
Adrianny <a.ingridreis@gmail.com>
2017-07-31 21:40:55 -0300

    tests

---
+++ tests/TestAno.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+class TestAno
+{
+    public static function bissexto($ano)
+    {
+        
+    }
+}
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -5,16 +5,49 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestAno;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    //caso 1 - condicao ano <= 0 false
+    public function testCase1()
     {
-        $this->assertTrue(true);
+        $response = TestAno::bissexto(0);
+        $this->assertFalse($response);
+    }
+
+    //caso 2 - condicao ano % 4 == 0 and ano % 100 != 0
+    public function testCase2()
+    {
+        $response = TestAno::bissexto(1424);
+        $this->assertTrue($response);
+    }
+
+    //caso 3 - condicao ano % 4 != 0
+    public function testCase3()
+    {
+        $response = TestAno::bissexto(1750);
+        $this->assertFalse($response);
+    }
+
+    //caso 4 - condicao ano % 100 == 0
+    public function testCase4()
+    {
+        $response = TestAno::bissexto(2500);
+        $this->assertFalse($response);
+    }
+
+    //caso 5 - condicao ano % 400 == 0
+    public function testCase5()
+    {
+        $response = TestAno::bissexto(1600);
+        $this->assertTrue($response);
+    }
+
+    //caso 6 - condicao ano % 400 != 0
+    public function testCase6()
+    {
+        $response = TestAno::bissexto(1400);
+        $this->assertFalse($response);
     }
 }
